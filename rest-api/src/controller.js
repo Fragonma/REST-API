2557144bf1274ad88d1d9ea331016772354756c5
Fragonma <franco_grosso2010@hotmail.com>
2023-10-20 16:11:50 -0300

    controlador y ruta de creacion de libro creado

--- rest-api/src/controller.js
+++ rest-api/src/controller.js
@@ -16,6 +16,12 @@ class LibrosController{
         }
         res.json(result[0]);
     }
+
+    async add(req, res) {
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO Libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+        res.json({'Id insertado': result.insertId})
+    }
 }
 
 export const libro = new LibrosController();
