bcb9b2c8d89401ee67478d966ac88e8b14965eb2
Fragonma <franco_grosso2010@hotmail.com>
2023-10-23 15:17:30 -0300

    Manejo de excepciones(try-catch) creado

--- rest-api/src/controller.js
+++ rest-api/src/controller.js
@@ -18,21 +18,36 @@ class LibrosController{
     }
 
     async add(req, res) {
-        const libro = req.body;
-        const [result] = await pool.query(`INSERT INTO Libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
-        res.json({'Id insertado': result.insertId})
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(`INSERT INTO Libros (nombre, autor, categoria, año_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN]);
+            res.json({'Id insertado': result.insertId});
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 
     async delete(req, res) {
-        const libro = req.body;
-        const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN = (?)`, [libro.ISBN]);
-        res.json({'Registro borrado': result.affectedRows}); 
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(`DELETE FROM Libros WHERE ISBN = (?)`, [libro.ISBN]);
+            res.json({'Registro borrado': result.affectedRows});
+        }
+        catch (error) {
+            console.log(error);
+        } 
     }
 
     async update(req, res) {
-        const libro = req.body;
-        const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
-        res.json({'Registros cambiados': result.changedRows});
+        try {
+            const libro = req.body;
+            const [result] = await pool.query(`UPDATE Libros SET nombre=(?), autor=(?), categoria=(?), año_publicacion=(?), ISBN=(?) WHERE id=(?)`, [libro.nombre, libro.autor, libro.categoria, libro.año_publicacion, libro.ISBN, libro.id]);
+            res.json({'Registros cambiados': result.changedRows});
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 }
 
